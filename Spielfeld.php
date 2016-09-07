8bee9058b501c2c6fa1c47ac3522445c0e8ef15b
SunnySunny93 <an.bo@live.de>
2016-09-07 15:35:04 +0200

    Mit Testdaten gefüllt

--- Spielfeld.php
+++ Spielfeld.php
@@ -7,38 +7,40 @@
 	<body>
 		<!-- III Spielinformationen -->
 		<?php
-			$spieler = "Du";
+			$partie = array("Du", "Tom", "Anna", "Julia", "Chris", "Trudi"); //Nur zum ausprobieren
+			$zahl = rand(0,5);
+			$spieler = $partie[$zahl];
 			if ($spieler == "Du") {
 				$zugausgabe = "Du bist am Zug";
 			} else {
 				$zugausgabe = "$spieler ist am Zug";
 			}
-			$ablauf = array(1,2,3,4);
+			$ablauf = rand(0,3);
 			//1: Karte ziehen, 2: Spielstein setzen, 3: Gegner schlagen, 4: Mauer setzen
 			if ($spieler == "Du"){
-				if ($ablauf[0]) {
+				if ($ablauf == 0) {
 					$ablauftext = "Ziehe eine Karte!";
 				}
-				if ($ablauf[1]) {
+				if ($ablauf == 1) {
 					$ablauftext = "Ziehe einen Stein!";
 				}
-				if ($ablauf[2]) {
+				if ($ablauf == 2) {
 					$ablauftext = "Schlage einen Gegner!";
 				}
-				if ($ablauf[3]) {
+				if ($ablauf == 3) {
 					$ablauftext = "Führe deine Mauerkarte aus!";
 				}
 			} else {
-				if ($ablauf[0]) {
+				if ($ablauf == 0) {
 					$ablauftext = "$spieler zieht eine Karte!";
 				}
-				if ($ablauf[1]) {
+				if ($ablauf == 1) {
 					$ablauftext = "$spieler zieht seinen Stein!";
 				}
-				if ($ablauf[2]) {
+				if ($ablauf == 2) {
 					$ablauftext = "$spieler schlägt einen Gegner!";
 				}
-				if ($ablauf[3]) {
+				if ($ablauf == 3) {
 					$ablauftext = "$spieler führt seine Mauerkarte aus!";
 				}
 			}
