e5e239be445f881174161ffea927ebe70f5db83b
SunnySunny93 <an.bo@live.de>
2016-09-07 15:23:59 +0200

    PHP Funktion eingefügt
    zur Bestimmung wer am Zug ist und was zu tun ist

--- Spielfeld.php
+++ Spielfeld.php
@@ -6,10 +6,47 @@
 	</head>
 	<body>
 		<!-- III Spielinformationen -->
-		<header>
-			<h2><label for="feedback">Du bist am Zug!</label> - <label for="ablauf">Ziehe eine Karte!</label><h2>
-		</header>
+		<?php
+			$spieler = "Du";
+			if ($spieler == "Du") {
+				$zugausgabe = "Du bist am Zug";
+			} else {
+				$zugausgabe = "$spieler ist am Zug";
+			}
+			$ablauf = array(1,2,3,4);
+			//1: Karte ziehen, 2: Spielstein setzen, 3: Gegner schlagen, 4: Mauer setzen
+			if ($spieler == "Du"){
+				if ($ablauf[0]) {
+					$ablauftext = "Ziehe eine Karte!";
+				}
+				if ($ablauf[1]) {
+					$ablauftext = "Ziehe einen Stein!";
+				}
+				if ($ablauf[2]) {
+					$ablauftext = "Schlage einen Gegner!";
+				}
+				if ($ablauf[3]) {
+					$ablauftext = "Führe deine Mauerkarte aus!";
+				}
+			} else {
+				if ($ablauf[0]) {
+					$ablauftext = "$spieler zieht eine Karte!";
+				}
+				if ($ablauf[1]) {
+					$ablauftext = "$spieler zieht seinen Stein!";
+				}
+				if ($ablauf[2]) {
+					$ablauftext = "$spieler schlägt einen Gegner!";
+				}
+				if ($ablauf[3]) {
+					$ablauftext = "$spieler führt seine Mauerkarte aus!";
+				}
+			}
 		
+		print "<header>";
+			print "<h2><label for='feedback'>$zugausgabe</label> - <label for='ablauf'>$ablauftext</label><h2>";
+		print "</header>";
+		?>
 		<!-- I Spielfeld -->
 		<center>
 			Hier entsteht das Spielfeld
