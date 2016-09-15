da5b21e26a4178b32b940f4cf2f687f58ece2eb1
SunnySunny93 <an.bo@live.de>
2016-09-15 10:54:30 +0200

    Spielfeld dynamisch erzeugen
    Obwohl ich mir immer noch nicht sicher bin ob eine Tabelle für ein
    Wabenförmiges Spielfeld sinnvoll ist habe ich dies nun Dynamisch
    aufgebaut und mit bespielbildern gefüllt.

--- Gruppen_de/Spielbrett.php
+++ Gruppen_de/Spielbrett.php
@@ -1,5 +1,28 @@
+<!-- Tabelle des Spielfelds dynamisch generiert -->
+<!-- Wie kann hier ein echtes Wabenmuster als Spielfeld entstehen? -->
+<?php
+	$i = 17;
+	print "<table border='0' width=60%>";
+	print "<colgroup width='100' span='17'></colgroup>";
+	while($i >0){
+		$j = 17;
+		print "<tr align='center' valign='center'>";
+		while($j >0){
+			if($i % 2 == 0){
+				print "<td><img src='/Bilder/Feld.png' width='50' border='0' alt='Feld'></td>"; //Nur zur Veranschaulichung gewählt
+			} else {
+				print "<td><img src='/Bilder/Feld2.png' width='50' border='0' alt='Feld'></td>";
+			}
+			$j -= 1;
+		}
+		print "</tr>";
+		$i -= 1;
+	}
+	print "</table>";
+	//while Schleife mit 2 Variablen zum Zählen der Zeilen und Spalten.
+?>
 
-<!-- Tabelle des Spielfelds -->
+<!-- Tabelle des Spielfelds statisch
 
 <table>
 	<tr>
@@ -54,3 +77,5 @@
 		<td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
 	</tr>
 </table>
+
+-->
