2cb24850fa54cff7ed544598aa2ebc279b0e916d
SunnySunny93 <an.bo@live.de>
2016-11-08 16:12:17 +0100

    Spielregeln nun erreichbar
    Mit zurück zum Spiel Button versehen

--- Gruppen_de/Optionen.php
+++ Gruppen_de/Optionen.php
@@ -1,3 +1,6 @@
 <!-- IV Einstellungen -->
 <!-- Hier können dann Optionen festgelegt werden, wie z.B. Hintergrundmusik ein und aus schalten.
 	 Mehr dazu später -->
+<form action= "Gruppen_de/Regeln.php">
+	<input type="submit" value="Zu den Regeln">
+</form>
