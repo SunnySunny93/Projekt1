a1293a63dee4adce239dddb98700868e1fc26752
SunnySunny93 <an.bo@live.de>
2016-11-09 12:03:19 +0100

    Spielstein funktioniert noch immer nicht
    php liefert kein Ergebnis zurück :(

--- Gruppen_de/spielstein.php
+++ Gruppen_de/spielstein.php
@@ -1,22 +1,21 @@
 <?php
-	//function spielstein($form, $farbe){
+	if (! empty($_POST['spielstein'] or $_POST['farbe'])){
+		header("Content-type: image/png");
+		$form = $_POST['spielstein'];
+		$farbe = $_POST['farbe'];
 		
-		print "<img src="
-			header("Content-type: image/png");
-			$form = .$_POST['spielstein'];
-			$farbe = .$_POST['farbe'];
-			
-			$image = imagecreate(200, 200);
-			$grau = imagecolorallocate( $image, 155,155,155);
-			if($form == 'kreis'){
-				imagefilledarc( $image, 100, 100, 50, 50, 0, 360, $farbe);
-			} else if($form == 'quadrat') {
-				imagefilledrectangle( $image, 50, 50, 150, 150, $farbe);
-			} else if($form =='dreieck') {
-				$points= array( 50, 100, 150, 25, 150, 75);
-				imagefilledpolygon( $image, $points, 3, $farbe);
-			}
-			imagepng $image;
-		"alt= 'Hier entsteht der Spielstein'>";
-	 //}
+		$image = imagecreate(200, 200);
+		$grau = imagecolorallocate( $image, 155,155,155);
+		if($form == 'kreis'){
+			imagefilledarc( $image, 100, 100, 50, 50, 0, 360, $farbe);
+		} else if($form == 'quadrat') {
+			imagefilledrectangle( $image, 50, 50, 150, 150, $farbe);
+		} else if($form =='dreieck') {
+			$points= array( 50, 100, 150, 25, 150, 75);
+			imagefilledpolygon( $image, $points, 3, $farbe);
+		}
+		imagepng($image);
+	}
 ?>
+
+<img src="spielstein.php" alt= "Ein Bild">
