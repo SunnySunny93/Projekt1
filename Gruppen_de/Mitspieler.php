3db267cc9ee020e67183873724fc5e74e226376d
SunnySunny93 <an.bo@live.de>
2017-01-09 16:50:31 +0100

    mitspieler

--- Gruppen_de/Mitspieler.php
+++ Gruppen_de/Mitspieler.php
@@ -1,24 +1,25 @@
 <!-- Mitspieler -->
+$this->spieler = $spieler; //link
 <table>
 	<tr>
 		<th>Nummer</th><th>Spieler</th>
 	</tr>
 	<tr>
-		<td>1</td><td></td>
+		<td>1</td><td>spieler[0].getName()</td>
 	</tr>
 	<tr>
-		<td>2</td><td></td>
+		<td>2</td><td>spieler[1].getName()</td>
 	</tr>
 	<tr>
-		<td>3</td><td></td>
+		<td>3</td><td>spieler[2].getName()</td>
 	</tr>
 	<tr>
-		<td>4</td><td></td>
+		<td>4</td><td>spieler[3].getName()</td>
 	</tr>
 	<tr>
-		<td>5</td><td></td>
+		<td>5</td><td>spieler[4].getName()</td>
 	</tr>
 	<tr>
-		<td>6</td><td></td>
+		<td>6</td><td>spieler[5].getName()</td>
 	</tr>
 </table>
