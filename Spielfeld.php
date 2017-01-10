d3fcda532f4f81396f8809589437796641dcc679
SunnySunny93 <an.bo@live.de>
2017-01-10 20:43:51 +0100

    Aufgeben Button angefangen

--- Spielfeld.php
+++ Spielfeld.php
@@ -5,6 +5,20 @@
 		<title>Spielfeld</title>
 	</head>
 	<body>
+		<button onclick="aufgeben()">Spiel aufgeben?</button>
+		<p id="demo"></p>
+
+		<script>
+			function aufgeben() {
+			    var x;
+			    if (confirm("Möchtest du wirklich die Partie beenden?") == true) {
+			        x = "You pressed OK!";			//link zu spielende.php
+			    } else {
+			        x = "You pressed Cancel!";	//fenster schließen ohne etwas zu tun
+			    }
+			    document.getElementById("demo").innerHTML = x;
+			}
+		</script>
 		<!-- III Spielinformationen -->
 		<?php include("Gruppen_de/Spielinformation.php");?>
 		<!-- I Spielfeld --><!--Karten -->
@@ -13,7 +27,7 @@
 			<td><?php include("Gruppen_de/Karten.php"); ?></td>
 			<td><?php include("Gruppen_de/Spielfeld.php"); ?></td>
 		</tr>
-		
+
 		Optionen
 		<?php include("Gruppen_de/Optionen.php"); ?>
 	</body>
