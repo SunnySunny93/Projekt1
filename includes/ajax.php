ca6d13f21508b492ac2f8477f5d6e1ba73c0244b
Jan Ryklikas <jry@facelift-bbt.com>
2017-01-11 17:54:44 +0100

    korrekturen

--- includes/ajax.php
+++ includes/ajax.php
@@ -6,9 +6,12 @@ if (isset($_SESSION['partie']))
     $content = file_get_contents('php://input');
     print_r($content);
     if($content != "") {
+        echo "\n";
         $json = json_decode($content);
-        $partie->getSpielfeld()->mauerAuswerten($json);
+        echo "test".$partie->getSpielfeld()->mauerAuswerten($json);
+        echo "\n";
         print_r($json);
+        echo "\n";
         print_r($partie->getSpielfeld()->felder);
     }else{
         echo json_encode($partie->karteAuswerten());
